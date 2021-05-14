9c411284201972e3484ef62b7f131dbdc5313955
Krzysztof Staszek <krzysztofstaszek@hotmail.com>
2021-05-14 20:20:36 +0200

    Dodaj pliki projektów.

---
+++ DataAccess/Repositories/SqlDataAccessRepository.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class SqlDataAccessRepository : ISqlDataAccess
+    {
+        private const string databaseName = "app.db";
+        public List<T> LoadData<T, U>(string sql, U parameters, CommandType type = CommandType.Text)
+        {
+            using (IDbConnection connection = new SQLiteConnection(GetConnectionString()))
+            {
+                var data = connection.Query<T>(sql, parameters, commandType: type);
+                return data.ToList();
+            }
+        }
+
+        public void SaveData<T>(string sql, T parameters, CommandType type = CommandType.Text)
+        {
+            using (IDbConnection connection = new SQLiteConnection(GetConnectionString()))
+            {
+                connection.Execute(sql, parameters, commandType: type);
+            }
+        }
+
+        private string GetConnectionString()
+        {
+            return $"Data Source={Environment.CurrentDirectory}\\{databaseName};";
+        }
+    }
+}
---
+++ Domain/Interfaces/ISqlDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface ISqlDataAccess
+    {
+        List<T> LoadData<T, U>(string sql, U parameters, CommandType type = CommandType.Text);
+        void SaveData<T>(string sql, T parameters, CommandType type = CommandType.Text);
+    }
+}
